603df623e95a6a4bbe5d5040a74dcaff0dde731d
Fatima Rizvi <fatimarzv23@gmail.com>
2020-08-12 12:42:50 -0500

    Task 7 complete

--- index.js
+++ index.js
@@ -158,12 +158,17 @@ DO NOT USE ADVANCED ARRAY METHODS (i.e. .filter) to solve this problem.
 
 hint - you can use the .includes method to help you solve this */
 
-function filterByWord(/*code here*/){
-
-    /*code here*/
-
+function filterByWord(arr, flavor) {
+    newArr = [];
+    for (let i = 0; i < arr.length; i++){
+        if (arr[i].includes(flavor)){
+            newArr.push(arr[i]);
+        }
+    }
+    return newArr;
 }
 
+console.log(filterByWord(originalFlavors,"Chocolate"));
 
 
 /* 🧁🍦🍨 STRETCH 🍨🍦🍫*/ 
